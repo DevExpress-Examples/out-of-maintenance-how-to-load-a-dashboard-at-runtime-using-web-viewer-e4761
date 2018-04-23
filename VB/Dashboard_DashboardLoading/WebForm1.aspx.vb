d47f9bb28277d2fef92bdf4ff977622c127b996b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Dashboard_DashboardLoading/WebForm1.aspx.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.IO
+Imports DevExpress.DataAccess.ConnectionParameters
+
+Namespace Dashboard_DashboardLoading
+	Partial Public Class WebForm1
+		Inherits System.Web.UI.Page
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+			' Includes the jQuery library in a web application.
+			ASPxDashboardViewer1.RegisterJQuery = True
+
+			' Specifies the dashboard identifier.
+			ASPxDashboardViewer1.DashboardId = "SalesDashboard"
+		End Sub
+
+		' Handles the DashboardLoading event.
+        Protected Sub ASPxDashboardViewer1_DashboardLoading( _
+                      ByVal sender As Object, _
+                      ByVal e As DevExpress.DashboardWeb.DashboardLoadingEventArgs)
+
+            ' Checks the identifier of the required dashboard.
+            If e.DashboardId = "SalesDashboard" Then
+
+                ' Writes the dashboard XML definition from a file to a string.
+                Dim definitionPath As String = Server.MapPath("App_Data/SalesDashboard.xml")
+                Dim dashboardDefinition As String = File.ReadAllText(definitionPath)
+
+                ' Specifies the dashboard XML definition.
+                e.DashboardXml = dashboardDefinition
+            End If
+        End Sub
+
+		' Handles the ConfigureDataConnection event.
+        Protected Sub ASPxDashboardViewer1_ConfigureDataConnection( _
+                      ByVal sender As Object, _
+                      ByVal e As DevExpress.DashboardWeb.ConfigureDataConnectionWebEventArgs)
+            If e.ConnectionName = "nwindConnection" Then
+
+                ' Gets connection parameters used to establish a connection to the database.
+                Dim parameters As Access97ConnectionParameters =
+                    CType(e.ConnectionParameters, Access97ConnectionParameters)
+                Dim databasePath As String = Server.MapPath("App_Data/nwind.mdb")
+
+                ' Specifies the path to a database file.                    
+                parameters.FileName = databasePath
+            End If
+        End Sub
+	End Class
+End Namespace
